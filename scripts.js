784c721552059a36ab3a981d948893579ad82b93
JJ <79755422+JJezorske@users.noreply.github.com>
2021-03-29 19:51:40 +0100

    first draft, sans responsivity

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const mixedMessages = {
+    _names: ['Porey', 'Peachy', 'Pui', 'Pandemoni', 'Desdemona'],
+    _verbs: ['adores', 'remembers', 'prefers', 'encourages', 'ignores'],
+    _nouns: ['apples!', 'treats!', 'groot?', 'snacks', 'capitalism' ],
+    getRandomWord(arr) {
+        let num = Math.floor(Math.random ()* (arr.length))
+        return arr[num]
+    },
+    generateMessage() {
+        const name = this.getRandomWord(this._names)
+        const verb = this.getRandomWord(this._verbs)
+        const noun = this.getRandomWord(this._nouns)
+        return `${name} ${verb} ${noun}`
+    },
+};
+
+
+  document.getElementById("btn").addEventListener("click", function (){
+    document.getElementById("message").innerHTML =  mixedMessages.generateMessage()
+});
